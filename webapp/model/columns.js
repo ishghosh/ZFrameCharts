f90860571b961e369718103ca1068b0b2a87cd87
ishghosh <ishanjgeccse16@gmail.com>
2021-02-08 08:46:28 +0000

    commit

---
+++ webapp/model/columns.js
@@ -0,0 +1,24 @@
+sap.ui.define([
+
+], function () {
+	"use strict";
+
+	return {
+		"cols": [{
+			"Label": "Product Id",
+			"Value": "ProductId"
+		}, {
+			"Label": "Category",
+			"Value": "Category"
+		}, {
+			"Label": "Main Category",
+			"Value": "MainCategory"
+		}, {
+			"Label": "Description",
+			"Value": "Description"
+		}, {
+			"Label": "Name",
+			"Value": "Name"
+		}]
+	};
+});
